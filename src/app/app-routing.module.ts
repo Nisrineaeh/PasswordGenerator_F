c240ba25a00472edb2fc72735a3dec7d0a6f4d66
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2024-01-17 12:15:52 +0100

    routerlink navbar ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { GeneratorComponent } from './pages/generator/generator.component';
+import { MyPasswordComponent } from './pages/my-password/my-password.component';
+import { AccountComponent } from './pages/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: "full" },
+  { path: 'home', component: AccueilComponent },
+  { path: 'generator', component: GeneratorComponent},
+  { path: 'my-password', component: MyPasswordComponent},
+  { path: 'my-account', component: AccountComponent},
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
